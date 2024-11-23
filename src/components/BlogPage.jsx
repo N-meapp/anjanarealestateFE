348c923edc51621fb2e320aacdd5220fb7deb2c2
N-meapp <developersnme@gmail.com>
2024-11-23 18:09:23 +0530

    hosting commit

---
+++ src/components/BlogPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import BlogContent from './BlogContent'
+
+const BlogPage = () => {
+  return (
+    <div>
+
+        <BlogContent />
+      
+    </div>
+  )
+}
+
+export default BlogPage
